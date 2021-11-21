533fdbe7ac6aae14de38990b94636478f0f508bd
David Omar Ghiano <davidghiano2@gmail.com>
2021-11-21 12:55:14 -0300

    Actualizacion

--- IEFI_Programacion/Iefi/src/config/Conexion.java
+++ IEFI_Programacion/Iefi/src/config/Conexion.java
@@ -7,25 +7,27 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class Conexion {
- public static String db = "iefi";
-        public static String user = "root";
-        public static String pass = "sol123456789";
-        public static String url = "jdbc:mysql://127.0.0.1:3306/iefi";
+    public static String db = "iefi";
+    public static String user = "root";
+    public static String pass = "sol123456789";
+    public static String url = "jdbc:mysql://127.0.0.1:3306/iefi";
+    
     Connection con;
+
     public Conexion(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-                con = DriverManager.getConnection(url, user, pass);
-            } catch (ClassNotFoundException | SQLException e) {
-                JOptionPane.showMessageDialog(null, "Error de Conexión"
-                        + ": " + e.getMessage());
-            }
+            con = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error de Conexión"
+                    + ": " + e.getMessage());
         }
+    }
         
     /*metodo para retornar el valor de la conexion*/
     
-       public Connection getConnection () {
-           return con;
+    public Connection getConnection () {
+        return con;
     }
             
             
